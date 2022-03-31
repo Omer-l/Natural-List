86cf4e7a39c5e541d3b2e5a4733d66423c18b633
Omer Kacar <56589837+Omer-l@users.noreply.github.com>
2022-03-31 10:13:02 +0100

    Update NaturalTest.java

--- src/NaturalTest.java
+++ src/NaturalTest.java
@@ -99,6 +99,26 @@ class NaturalTest {
 			n1.subtract(n2);
 		});
 	}
+
+	/** MULTIPLY */
+
+	@Test
+	void falsifyMultiplyPrecondition() {
+		assertThrows(PreconditionError.class, () -> {
+			Natural n1 = new Natural(Integer.MAX_VALUE);
+			Natural n2 = new Natural(0);
+			n1.multiply(n2);
+		});	
+	}
+
+	@Test
+	void satisfyMultiplyPostcondition() {
+		assertDoesNotThrow(() -> {
+			Natural n1 = new Natural(2);
+			Natural n2 = new Natural(Integer.MAX_VALUE / 2);
+			n1.multiply(n2);
+		});
+	}
 	
 	/** DIVIDE */
 	
